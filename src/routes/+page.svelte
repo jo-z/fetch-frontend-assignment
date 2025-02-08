425addd626ac2649a099258c94edb4fddb9e7653
Josephine Bartholoma <josephine.bartholoma@gmail.com>
2025-02-07 19:24:15 -0600

    fetch initial dog data

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,30 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
 	import { goto } from '$app/navigation';
+	import { api } from '$lib/constants';
 	let dogs = $state([]);
+	let next = $state('');
 	onMount(async () => {
 		try {
-			const response = await fetch('https://frontend-take-home-service.fetch.com/dogs/breeds', {
+			const searchResponse = await fetch(`${api}/dogs/search`, {
 				credentials: 'include'
 			});
-			console.log(response);
-			if (response.status === 401) goto('/login');
+			console.log(searchResponse);
+			if (searchResponse.status === 401) goto('/login');
+			else {
+				const searchResults = await searchResponse.json();
+				next = searchResults.next;
+				console.log('searchResults: ', searchResults);
+				const headers = new Headers();
+				headers.append('Content-Type', 'application/json');
+				const dogsResponse = await fetch(`${api}/dogs`, {
+					method: 'POST',
+					body: JSON.stringify(searchResults.resultIds),
+					credentials: 'include',
+					headers
+				});
+				dogs = await dogsResponse.json();
+			}
 		} catch (error) {}
 	});
 </script>
