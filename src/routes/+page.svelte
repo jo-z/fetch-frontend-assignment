6efeed71a84326d806312749c3239ec8998c5a30
Josephine Bartholoma <josephine.bartholoma@gmail.com>
2025-02-09 17:28:51 -0600

    add filtering by breeds

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,33 +4,53 @@
 	import { api } from '$lib/constants';
 	import DogBox from './dog.svelte';
 	import { type Dog } from '$lib/interfaces';
+	import Filter from './filter.svelte';
 	let dogs: Array<Dog> = $state([]);
+	let breeds: Array<string> = $state([]);
+	let selectedBreeds = $state([]);
 	let next = $state('');
+
+	const search = async () => {
+		const searchResponse = await fetch(
+			`${api}/dogs/search?${selectedBreeds.length ? selectedBreeds.map((breed) => `breeds=${breed}`).join('&') : ''}`,
+			{
+				credentials: 'include'
+			}
+		);
+		console.log(searchResponse);
+		if (searchResponse.status === 401) goto('/login');
+		else {
+			const searchResults = await searchResponse.json();
+			next = searchResults.next;
+			console.log('searchResults: ', searchResults);
+			const headers = new Headers();
+			headers.append('Content-Type', 'application/json');
+			const dogsResponse = await fetch(`${api}/dogs`, {
+				method: 'POST',
+				body: JSON.stringify(searchResults.resultIds),
+				credentials: 'include',
+				headers
+			});
+			dogs = await dogsResponse.json();
+		}
+	};
+
 	onMount(async () => {
 		try {
-			const searchResponse = await fetch(`${api}/dogs/search`, {
-				credentials: 'include'
-			});
-			console.log(searchResponse);
-			if (searchResponse.status === 401) goto('/login');
-			else {
-				const searchResults = await searchResponse.json();
-				next = searchResults.next;
-				console.log('searchResults: ', searchResults);
-				const headers = new Headers();
-				headers.append('Content-Type', 'application/json');
-				const dogsResponse = await fetch(`${api}/dogs`, {
-					method: 'POST',
-					body: JSON.stringify(searchResults.resultIds),
-					credentials: 'include',
-					headers
-				});
-				dogs = await dogsResponse.json();
-			}
+			search();
+			const breedsResponse = await fetch(`${api}/dogs/breeds`, { credentials: 'include' });
+			breeds = await breedsResponse.json();
 		} catch (error) {}
 	});
 </script>
 
+<Filter
+	{breeds}
+	bind:selectedBreeds
+	searchOnClick={() => {
+		search();
+	}}
+/>
 <div class="dog-container">
 	{#each dogs as dogInfo}
 		<DogBox {...dogInfo} />
---
+++ src/routes/filter.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+	let {
+		breeds,
+		selectedBreeds = $bindable([]),
+		searchOnClick
+	}: {
+		breeds: Array<string>;
+		selectedBreeds: Array<string>;
+		searchOnClick: () => void;
+	} = $props();
+</script>
+
+<div>
+	<form name="search and filter">
+		<label for="breeds">Breeds:</label>
+		<select id="breeds" multiple bind:value={selectedBreeds} size="10">
+			{#each breeds as breed}
+				<option value={breed}>{breed}</option>
+			{/each}
+		</select>
+		<button
+			onclick={() => {
+				searchOnClick();
+				console.log('component selected breeds: ', $state.snapshot(selectedBreeds));
+			}}>Search</button
+		>
+	</form>
+</div>
+
+<style>
+</style>
