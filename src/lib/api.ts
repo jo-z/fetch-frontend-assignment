978c31ddc173c0f524211f46a238fe6f7c247c8a
Josephine Bartholoma <josephine.bartholoma@gmail.com>
2025-02-13 01:26:13 -0600

    move redirect on 401 to new redirectFetch function

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -3,31 +3,33 @@ import type { Dog, Location, LocationSearchBody } from './interfaces';
 import { goto } from '$app/navigation';
 const headers = new Headers();
 headers.append('Content-Type', 'application/json');
-
+const redirectFetch = async (...args: [RequestInfo | URL, init?: RequestInit]) => {
+	const response = await fetch(...args);
+	if (response.status === 401) goto('/login');
+	return response;
+};
 /***
  * Takes an array of zipcode strings and returns an array of Location objects
  * @returns Array<Location>
  */
 export const getLocations = async (zipcodes: Array<string>) => {
-	const locationResponse = await fetch(`${api}/locations`, {
+	const locationResponse = await redirectFetch(`${api}/locations`, {
 		credentials: 'include',
 		body: JSON.stringify(zipcodes),
 		headers,
 		method: 'POST'
 	});
-	if (locationResponse.status === 401) goto('/login');
 	const body: Array<Location> = await locationResponse.json();
 	return body;
 };
 
 export const searchLocations = async (body: LocationSearchBody) => {
-	const searchResponse = await fetch(`${api}/locations/search`, {
+	const searchResponse = await redirectFetch(`${api}/locations/search`, {
 		credentials: 'include',
 		body: JSON.stringify(body),
 		headers,
 		method: 'POST'
 	});
-	if (searchResponse.status === 401) goto('/login');
 	const results: {
 		results: Location[];
 		total: number;
@@ -50,43 +52,38 @@ export const searchDogs = async ({
 	currentPage: number;
 	zipCodes?: Array<string>;
 }) => {
-	const searchResponse = await fetch(
+	const searchResponse = await redirectFetch(
 		`${api}/dogs/search?${breeds?.length ? breeds.map((breed) => `breeds=${breed}`).join('&') : ''}&sort=${sortField}:${sortDirection}&size=${perPage}&from=${(currentPage - 1) * perPage}${zipCodes?.length ? `&${zipCodes.map((zipCode) => `zipCodes=${zipCode}`).join('&')}` : ''}`,
 		{
 			credentials: 'include'
 		}
 	);
-	if (searchResponse.status === 401) goto('/login');
 	const body: { resultIds: Array<string>; total: number; next?: string; prev?: string } =
 		await searchResponse.json();
 	return body;
 };
 
 export const searchDogsWithString = async (searchString: string) => {
-	const response = await fetch(`${api}${searchString}`, { credentials: 'include' });
-	if (response.status === 401) goto('/login');
+	const response = await redirectFetch(`${api}${searchString}`, { credentials: 'include' });
 	const body: { resultIds: Array<string>; total: number; next?: string; prev?: string } =
 		await response.json();
 	return body;
 };
 
 export const getDogs = async (dogIds: Array<string>) => {
-	console.log('hello 1');
-	const dogsResponse = await fetch(`${api}/dogs`, {
+	const dogsResponse = await redirectFetch(`${api}/dogs`, {
 		method: 'POST',
 		body: JSON.stringify(dogIds),
 		credentials: 'include',
 		headers
 	});
-	console.log('hello 2');
-	if (dogsResponse.status === 401) goto('/login');
-	console.log(dogsResponse);
+	// if (dogsResponse.status === 401) goto('/login');
 	const body: Array<Dog> = await dogsResponse.json();
 	return body;
 };
 
 export const getBreeds = async () => {
-	const breedsResponse = await fetch(`${api}/dogs/breeds`, { credentials: 'include' });
+	const breedsResponse = await redirectFetch(`${api}/dogs/breeds`, { credentials: 'include' });
 	const breeds: Array<string> = await breedsResponse.json();
 	return breeds;
 };
@@ -103,19 +100,18 @@ export const login = async ({ name, email }: { name: string; email: string }) =>
 };
 
 export const getMatch = async (dogIds: Array<string>) => {
-	const response = await fetch(`${api}/dogs/match`, {
+	const response = await redirectFetch(`${api}/dogs/match`, {
 		credentials: 'include',
 		method: 'POST',
 		body: JSON.stringify(dogIds),
 		headers
 	});
-	if (response.status === 401) goto('/');
 	const body: { match: string } = await response.json();
 	return body;
 };
 
 export const logout = async () => {
-	await fetch(`${api}/auth/logout`, {
+	await redirectFetch(`${api}/auth/logout`, {
 		credentials: 'include',
 		method: 'POST'
 	});
